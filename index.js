88f262de2c70c80d5eeb3b889fe9d4cc28fb1690
afonsomarquesEh9 <afonso-0@hotmail.com>
2025-07-01 19:19:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById('meuBotao');
+const mensagem = document.getElementById('mensagem');
+
+botao.addEventListener('click', function () {
+    mensagem.style.display = 'block';
+});
